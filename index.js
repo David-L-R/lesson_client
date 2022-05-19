e8d5ad2fc9166923c973fe846dfdc80ff6585b58
David Rajcher <davidrajcher@Davids-MBP-2.codaisseur.local>
2022-05-19 11:01:52 +0200

    cleaned

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require("express");
-const users = require("./users.json");
-
-const app = express();
-
 // GET all users
 
 // GET user :id
-
-const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`server is running on port: ${PORT}`);
-});
